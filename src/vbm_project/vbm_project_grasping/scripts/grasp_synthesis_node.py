b2cda94bac8f0b75188fc31a26af419a2be5f217
hrishikesh-st <hpawar@wpi.edu>
2023-10-13 17:35:00 -0400

    Added comments

--- src/vbm_project/vbm_project_grasping/scripts/grasp_synthesis_node.py
+++ src/vbm_project/vbm_project_grasping/scripts/grasp_synthesis_node.py
@@ -13,6 +13,22 @@ from sklearn.decomposition import PCA
 
 
 class GraspSynthesis(Node):
+    """ Grasp synthesis node
+
+    Args:
+
+    Attributes:
+        processed_pointcloud_subscriber (rclpy.Subscriber): Subscribe to processed point cloud
+        thresh_pc_publisher (rclpy.Publisher): Publish thresholded point cloud
+        gp_publisher (rclpy.Publisher): Publish grasping points marker
+        threshold (list): Threshold for point cloud (lower and upper limit)
+        grasping_points (visualization_msgs.msg.Marker): Marker for grasping points
+
+    Methods:
+        processed_pointcloud_cb(self, msg): Callback function for processed point cloud
+        _init_marker(self, marker, points): Initialize marker
+    """
+
     def __init__(self):
         super().__init__('grasp_points_systhesis')
 
@@ -23,58 +39,83 @@ class GraspSynthesis(Node):
             10
         )
         self.processed_pointcloud_subscriber
-        self.pc_publisher= self.create_publisher(PointCloud2, "/thresholded_pointcloud", 10)
+        self.thresh_pc_publisher= self.create_publisher(PointCloud2, "/thresholded_pointcloud", 10)
         self.gp_publisher= self.create_publisher(Marker, "/grasping_points", 10)
 
+        # Threshold for point cloud (lower and upper limit)
         self.threshold = [0.012, 0.018] # hammer
         # self.threshold = [0.005, 0.01] # drill, coke
 
+        # Grasping points are visualized in RViz using visualization_msgs.msg.Marker
+        # the SPHERE_LIST marker type is used as it can visualize multiple points in the single marker message
         self.grasping_points = Marker()
         self.grasping_point1 = Point()
         self.grasping_point2 = Point()
-        self.id = 0
 
     def _init_marker(self, marker, points):
-        marker.header.frame_id = "world"
-        marker.type = marker.SPHERE_LIST
+        """ Initialize and populate Marker message fields
+
+        Args:
+            marker (visualization_msgs.msg.Marker): Marker
+            points (list): List of points
+
+        Returns:
+            None
+        """
+
+        marker.header.frame_id = "world" # Marker Frame
+        marker.type = marker.SPHERE_LIST # Marker Type
         marker.action = marker.ADD
+
+        # Marker size
         marker.scale.x = 0.01
         marker.scale.y = 0.01
         marker.scale.z = 0.01
+
+        # Marker color
         marker.color.r = 0.0
         marker.color.g = 1.0
         marker.color.b = 0.0
         marker.color.a = 1.0
+
+        # Marker positions and orientation
         marker.pose.orientation.w = 1.0
         marker.points = points
 
     def processed_pointcloud_cb(self, msg):
+        """ Callback function for processed point cloud
+        This function performs thresholding on processed pointcloud to get the top surface
+        of the object and then calculates the grasping points on the object based on the
+        grasping heuristic.
+
+        Args:
+            msg (sensor_msgs.msg.PointCloud2): Point cloud
+
+        Returns:
+            None
+        """
 
         processed_pointcloud = point_cloud2.read_points_numpy(msg)
 
-        _coords = processed_pointcloud[processed_pointcloud[:, 2].argsort()] # Ascending order
-        
+        # all points of the point cloud are arranged in the ascending order of z coordinates
+        _coords = processed_pointcloud[processed_pointcloud[:, 2].argsort()]
+
+        # Thresholding point cloud to get the top surface of the object (layer of points)
+        # All points below the lower limit are discarded
         lower_limit = _coords[:, 2][-1] - self.threshold[1]
         i = np.argwhere(_coords[:, 2] > lower_limit)
         thresh_coords1 = _coords[i].squeeze()
 
+        # All points above the upper limit are discarded
         upper_limit = thresh_coords1[:, 2][-1] - self.threshold[0]
         i = np.argwhere(thresh_coords1[:, 2] < upper_limit)
         thresh_coords2 = thresh_coords1[i].squeeze()
 
-        # Flattened array
+        # Flattened array, remove z coordinates
         flatten_coords = thresh_coords2[:, :2]
-        zeros_column = np.zeros((flatten_coords.shape[0], 1))
-        modified_falttened_coords = np.hstack((flatten_coords, zeros_column))
-
-        # np.save('hammer.npy', flatten_coords)
-        # print("Saved numpy file")
-
-        # calculate normals
-        # Define the number of neighbors to consider (k)
-        k = 3
 
         # Function to find the normal at each point
+        k = 3 # Number of neighbors to calculate normal
         def calculate_normals(point_cloud, k):
             normals = []
             for point in point_cloud:
@@ -93,6 +134,8 @@ class GraspSynthesis(Node):
 
         # Calculate normals for each point
         normal_vectors = calculate_normals(flatten_coords, k)
+
+        # calculate center of thresholded point cloud
         _center = [np.mean(flatten_coords[:, 0]), np.mean(flatten_coords[:, 1])]
 
         # calculate points for grasping
@@ -100,21 +143,27 @@ class GraspSynthesis(Node):
         _point1 = None # 2d
         normal1 = None
 
+        # find point closest to center, and its corresponding normal (Point 1)
         for point, normal in zip(flatten_coords, normal_vectors):
             _dist = np.sqrt((point[0] - _center[0])**2 + (point[1] - _center[1])**2)
-            if _dist < min_dist: 
+            if _dist < min_dist:
                 min_dist = _dist
                 _point1 = point.copy()
                 normal1 = normal.copy()
-        
+
+        # find point which satisfies the grasping heuristic (Point 2)
+        # The angles between the vector connecting the 2 grasping points and the corresponding normals
+        # should be between 0 to 10 or 170 to 180 degrees
+        # Both these limits are considered because the directions of the normals can be flipped
         _point2 = None # 2d
         for point, normal in zip(flatten_coords, normal_vectors):
             _dist = np.sqrt((point[0] - _point1[0])**2 + (point[1] - _point1[1])**2)
-            if _dist == 0: continue
-            _vec = np.array([(point[0]-_point1[0])/_dist, (point[1]-_point1[1])/_dist]) # unit vector
+            if _dist == 0: continue # skip if point is same as point1
+            # unit vector between point and point1
+            _vec = np.array([(point[0]-_point1[0])/_dist, (point[1]-_point1[1])/_dist])
 
-            _deg1 = np.degrees(np.arccos(np.dot(_vec, normal1)))
-            _deg2 = np.degrees(np.arccos(np.dot(_vec, normal)))
+            _deg1 = np.degrees(np.arccos(np.dot(_vec, normal1))) # Angle between vector and normal1 (point1), degrees
+            _deg2 = np.degrees(np.arccos(np.dot(_vec, normal))) # Angle between vector and normal (point), degrees
 
             flag1 = False
             flag2 = False
@@ -130,12 +179,14 @@ class GraspSynthesis(Node):
         point1 = [0.0, 0.0, 0.0]
         point2 = [0.0, 0.0, 0.0]
 
+        # quering 3d points from pointcloud corresponding to 2d grasping points
         for point in thresh_coords2:
             if point[0] == _point1[0] and point[1] == _point1[1]:
                 point1 = point.copy()
             if point[0] == _point2[0] and point[1] == _point2[1]:
                 point2 = point.copy()
 
+        # Converting points 1 and 2 to geometry_msgs.msg.Point
         self.grasping_point1.x = float(point1[0])
         self.grasping_point1.y = float(point1[1])
         self.grasping_point1.z = float(point1[2])
@@ -144,8 +195,9 @@ class GraspSynthesis(Node):
         self.grasping_point2.y = float(point2[1])
         self.grasping_point2.z = float(point2[2])
 
-        _grasping_points = [self.grasping_point1, self.grasping_point2]
+        _grasping_points = [self.grasping_point1, self.grasping_point2] # List of grasping points
 
+        # initialize grasping_points Marker message with list of grasping points
         self._init_marker(self.grasping_points, _grasping_points)
 
         fields = [PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
@@ -155,10 +207,10 @@ class GraspSynthesis(Node):
         header = Header()
         header.frame_id = "world"
         header.stamp = self.get_clock().now().to_msg()
-    
+
         pc2 = point_cloud2.create_cloud(header, fields, thresh_coords2)
-        self.pc_publisher.publish(pc2)
-        self.gp_publisher.publish(self.grasping_points)
+        self.thresh_pc_publisher.publish(pc2) # publish thresholded point cloud
+        self.gp_publisher.publish(self.grasping_points) # publish grasping points marker
 
 
 def main(args=None):
@@ -166,8 +218,8 @@ def main(args=None):
     image_subscriber = GraspSynthesis()
     rclpy.spin(image_subscriber)
     image_subscriber.destroy_node()
-    
+
     rclpy.shutdown()
-  
+
 if __name__ == '__main__':
     main()
--- src/vbm_project/vbm_project_grasping/scripts/transform_point_cloud.py
+++ src/vbm_project/vbm_project_grasping/scripts/transform_point_cloud.py
@@ -6,33 +6,55 @@ from std_msgs.msg import Header
 from sensor_msgs.msg import Image # Image is the message type
 from sensor_msgs.msg import PointCloud2, PointField
 from sensor_msgs_py import point_cloud2
-from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import numpy as np
 
 
 class PointCloudTransformation(Node):
+    """ Point cloud transformation node
+
+    Attributes:
+        pc_subscriber (rclpy.Subscriber): Subscribe to raw point cloud
+        transformed_pc_publisher (rclpy.Publisher): Publish transformed point cloud
+
+    Methods:
+        pointcloud_cb(self, data): Callback function for point cloud
+    """
+
     def __init__(self):
         super().__init__('image_subscriber')
 
-        self.subscription_pointcloud = self.create_subscription(
+        self.pc_subscriber = self.create_subscription(
             PointCloud2,
             '/realsense/points',
-            self.transformed_pointcloud_cb,
+            self.pointcloud_cb,
             10
         )
-        self.subscription_pointcloud
-        self.pc_publisher= self.create_publisher(PointCloud2, "/transformed_pointcloud_data", 10)
+        self.pc_subscriber
+        self.transformed_pc_publisher= self.create_publisher(PointCloud2, "/transformed_pointcloud_data", 10)
 
-    def transformed_pointcloud_cb(self, data):
+    def pointcloud_cb(self, data):
+        """ Callback function for point cloud
+
+        Args:
+            data (sensor_msgs.msg.PointCloud2): Point cloud
+
+        Returns:
+            None
+        """
 
         points_in_cam_frame = point_cloud2.read_points_numpy(data) # 307200 x 4
+
+        # Separate xyz points and intensity
         _cam_data = points_in_cam_frame[:, :3]
         _cam_int = points_in_cam_frame[:, 3]
 
+        # Replace intensity values with 1 to perform transformation
         _cam_data = np.hstack((_cam_data, np.ones_like(_cam_int).reshape(-1, 1)))
 
+        # Transformation matrix from camera frame to world frame
         tf_rot = np.array([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, -1, 1.5], [0, 0, 0, 1]])
 
+        # Transforming points to world frame
         _world_data = np.matmul(tf_rot, _cam_data.T)
         points_in_world_frame = np.hstack((_world_data.T[:, :3], _cam_int.reshape(-1, 1)))
 
@@ -44,18 +66,18 @@ class PointCloudTransformation(Node):
         header = Header()
         header.frame_id = "world"
         header.stamp = self.get_clock().now().to_msg()
-    
-        pc2 = point_cloud2.create_cloud(header, fields, points_in_world_frame)
-        self.pc_publisher.publish(pc2)
 
-        
+        pc2 = point_cloud2.create_cloud(header, fields, points_in_world_frame)
+        self.transformed_pc_publisher.publish(pc2) # Publish the transformed point cloud
+
+
 def main(args=None):
     rclpy.init(args=args)
     image_subscriber = PointCloudTransformation()
     rclpy.spin(image_subscriber)
     image_subscriber.destroy_node()
-    
+
     rclpy.shutdown()
-  
+
 if __name__ == '__main__':
     main()
